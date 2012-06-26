06381b9ece0d9c8b83853b477339e66bfcda4082
Alex Nichol <alexnichol@comcast.net>
2012-06-26 10:42:45 -0400

    Initial Commit

---
+++ AN3D/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  AN3D
+//
+//  Created by Alex Nichol on 6/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
